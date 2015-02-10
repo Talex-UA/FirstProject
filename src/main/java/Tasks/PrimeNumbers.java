06d85a5e6ec62ab8d7d7b0c76d9649630ba388b9
Talex-UA <talexua91@gmail.com>
2015-02-10 19:07:29 +0200

    Updated: Prime Numbers task

--- src/main/java/Tasks/PrimeNumbers.java
+++ src/main/java/Tasks/PrimeNumbers.java
@@ -1,33 +1,40 @@
 package Tasks;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class PrimeNumbers {
     public static void main(String args[]) throws java.io.IOException {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please, enter your number.");
         int number = 0;
         while (true) {
             try {
+                System.out.println("Please, enter your number.");
                 number = input.nextInt();
-                if (number < 2) throw new RuntimeException();
-                break;
-            } catch (Exception e) {
-                System.out.println("Please, try again");
-                input.skip(".*");
             }
+
+            catch (InputMismatchException e){}
+            catch (NoSuchElementException | IllegalStateException e) {
+                System.out.println("Something went wrong, closing");
+                return;
+            }
+
+            if (number >= 2) break;
+
+            System.out.println("Please, try again");
+            input.skip(".*");
         }
 
-        for (int j = 2; j < number + 1; j++) {
-            if (j % 2 != 0 || j==2) {
-                int x = (int) Math.sqrt(j);
-                int count = 0;
+        System.out.println("2");
+        for (int j = 3; j < number + 1; j=j+2) {
+            finder:{
+            int x = (int) Math.sqrt(j);
                 for (int k = x; k > 1; k--) {
-                    if (j % k == 0) count++;
+                    if (j % k == 0) break finder;
                 }
-                if (count == 0) System.out.println(j);
+            System.out.println(j);
             }
-
         }
     }
 }
