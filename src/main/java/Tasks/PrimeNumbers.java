8a25916a4603ee0bf04382948ccfa06ca31c80eb
Talex-UA <talexua91@gmail.com>
2015-02-11 12:15:27 +0200

    Prime Numbers task

--- src/main/java/Tasks/PrimeNumbers.java
+++ src/main/java/Tasks/PrimeNumbers.java
@@ -8,9 +8,10 @@ public class PrimeNumbers {
     public static void main(String args[]) throws java.io.IOException {
         Scanner input = new Scanner(System.in);
         int number = 0;
-        while (true) {
+        System.out.println("Please, enter your number.");
+
+        while (number==0) {
             try {
-                System.out.println("Please, enter your number.");
                 number = input.nextInt();
             }
 
@@ -24,13 +25,14 @@ public class PrimeNumbers {
 
             System.out.println("Please, try again");
             input.skip(".*");
+            number=0;
         }
 
         System.out.println("2");
         for (int j = 3; j < number + 1; j=j+2) {
             finder:{
-            int x = (int) Math.sqrt(j);
-                for (int k = x; k > 1; k--) {
+            int root = (int) Math.sqrt(j);
+                for (int k = root; k > 1; k--) {
                     if (j % k == 0) break finder;
                 }
             System.out.println(j);
