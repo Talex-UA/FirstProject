2d48c08126da824bc06d05ef351c6d87016a0a33
Talex-UA <talexua91@gmail.com>
2015-02-13 15:44:38 +0200

    Updated

---
+++ src/main/java/TasksWithStaticMethods/UserInput.java
@@ -0,0 +1,13 @@
+package TasksWithStaticMethods;
+
+public class UserInput {
+    String input1;
+    String input2;
+
+    UserInput (){}
+
+    UserInput(String input1,String input2){
+        this.input1=input1;
+        this.input2=input2;
+    }
+}
