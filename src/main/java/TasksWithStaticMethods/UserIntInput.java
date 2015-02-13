6e8debc09843dad171103fd828621707d5a3aac1
Talex-UA <talexua91@gmail.com>
2015-02-13 15:44:49 +0200

    Updated

---
+++ src/main/java/TasksWithStaticMethods/UserIntInput.java
@@ -0,0 +1,15 @@
+package TasksWithStaticMethods;
+
+public class UserIntInput {
+    int year;
+    int month;
+    int day;
+
+    UserIntInput(){}
+
+    UserIntInput(int year, int month, int day){
+        this.day=day;
+        this.month=month;
+        this.year=year;
+    }
+}
