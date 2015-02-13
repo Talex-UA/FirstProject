e55faf1da0f1a2b54e2c4aa7b3958076b0882e55
Talex-UA <talexua91@gmail.com>
2015-02-13 15:21:27 +0200

    Updated

--- src/main/java/TasksWithStaticMethods/Anagram_4methods.java
+++ src/main/java/TasksWithStaticMethods/Anagram_4methods.java
@@ -7,11 +7,7 @@ import java.util.Scanner;
 public class Anagram_4methods {
     public static void main(String[] args) {
 
-        UserIput input = new UserIput();
-
-        input= getString(input);
-        boolean res = areAnagrams(input);
-        printResult(res);
+        printResult(areAnagrams(getString()));
     }
 
     public static void printResult(boolean isAnagram) {
@@ -20,25 +16,27 @@ public class Anagram_4methods {
         } else System.out.println("These phrases are NOT anagrams");
     }
 
-    public static UserIput getString(UserIput userIput) {
+    public static UserInput getString() {
         Scanner input = new Scanner(System.in);
+        UserInput userInput = new UserInput();
         System.out.println("Enter first phrase");
-        userIput.input1=input.nextLine();
+        userInput.input1=input.nextLine();
         System.out.println("Enter second phrase");
-        userIput.input2=input.nextLine();
-        return userIput;
+        userInput.input2=input.nextLine();
+        return userInput;
     }
 
-    public static boolean areAnagrams(UserIput userIput){
-        char [] array1 = userIput.input1.toLowerCase().replaceAll("\\s+","").toCharArray();
-        char [] array2 = userIput.input2.toLowerCase().replaceAll("\\s+","").toCharArray();
+    public static boolean areAnagrams(UserInput userInput){
+        char [] array1 = userInput.input1.toLowerCase().replaceAll("\\s+","").toCharArray();
+        char [] array2 = userInput.input2.toLowerCase().replaceAll("\\s+","").toCharArray();
         Arrays.sort(array1);
         Arrays.sort(array2);
         return Arrays.equals(array1, array2);
     }
+    static class UserInput {
+        String input1;
+        String input2;
+    }
+
 }
 
-class UserIput{
-    String input1;
-    String input2;
-}
